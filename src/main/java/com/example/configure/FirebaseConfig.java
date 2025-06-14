67f92388275175ab22b558030f5a3049a959fdb5
Eaiqrazz <eaiqrazzpc@gmail.com>
2025-06-14 15:57:49 +0530

    update the code

--- src/main/java/com/example/configure/FirebaseConfig.java
+++ src/main/java/com/example/configure/FirebaseConfig.java
@@ -1,37 +1,41 @@
 package com.example.configure;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-
-import org.springframework.context.annotation.Configuration;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
+	@Value("${FIREBASE_CONFIG}")
+	private String firebaseConfig;
+
 	@PostConstruct
 	public void init() {
 		try {
-			String firebaseJson = System.getenv("FIREBASE_CONFIG");
+			if (firebaseConfig == null || firebaseConfig.trim().isEmpty()) {
+				throw new IllegalStateException("FIREBASE_CONFIG ");
+			}
 
-			InputStream serviceAccount = new ByteArrayInputStream(firebaseJson.getBytes(StandardCharsets.UTF_8));
+			InputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes(StandardCharsets.UTF_8));
 
 			FirebaseOptions options = new FirebaseOptions.Builder()
 					.setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
 
 			if (FirebaseApp.getApps().isEmpty()) {
 				FirebaseApp.initializeApp(options);
+				System.out.println("✅ Firebase initialized successfully.");
 			}
 		} catch (Exception e) {
+			System.err.println("❌ Firebase initialization failed:");
 			e.printStackTrace();
 		}
 	}
-
 }
